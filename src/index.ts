fed61af2fb00a66eb5dd92e923cdd8f5065e4a55
EmilyKali <adharaultimasheireen@gmail.com>
2024-01-29 04:12:45 +0100

    better exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import * as cacheElement from "./models/CacheElement";
-import * as time from "./models/Time";
-import * as genericCache from "./caches/GenericCache";
-
-export const CacheElement = cacheElement.CacheElement;
-export const Time = time.Time;
-export type TimeString = time.TimeString;
-export type GenericCache = genericCache.GenericCacheOptions;
-export type SizeExceededStrategy = genericCache.SizeExceededStrategy;
+export { CacheElement, Expiration } from "./models/CacheElement";
+export { Time, TimeConstructor, TimeString } from "./models/Time";
+export { GenericCache, GenericCacheOptions, SizeExceededStrategy, defaultGenericCacheOptions } from "./caches/GenericCache";
