e1c0df1977378c7ea146a0c12c548324d7bacb4c
Emily <laura.emily.lovi@gmail.com>
2024-02-03 00:40:16 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,11 @@
 export { EpicCache, LayerBehavior } from './components/Cache';
 export { Layer, LayerOptions } from './components/Layer';
 export { BetterEmitter, EventHandler } from './components/BetterEmitter';
+
+export { CacheElement, Expiration } from './models/CacheElement';
+export { Time, TimeConstructor, TimeString } from './models/Time';
+
+export { Store } from './interfaces/Store';
+
+export { FileStore, FileStoreOptions } from './stores/FileStore';
+export { MemoryStore } from './stores/MemoryStore';
